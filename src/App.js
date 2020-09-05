f97945ee3d96b33a517fe07d9c48fbe0bcf493b5
pawel <p2vvel@gmail.com>
2020-09-05 21:53:48 +0200

    fixed API connections

--- src/App.js
+++ src/App.js
@@ -37,66 +37,93 @@ class App extends React.Component {
 		this.handleLocationSearchChange = this.handleLocationSearchChange.bind(this);
 		this.handleLocationSearchSubmit = this.handleLocationSearchSubmit.bind(this);
 		this.handleAdvanceSearchCancel = this.handleAdvanceSearchCancel.bind(this);
-		this.handleAdvanceSearchSave = this.handleAdvanceSearchSave.bind(this);
+		this.handleAdvanceSearchSubmit = this.handleAdvanceSearchSubmit.bind(this);
 		this.handleAdvanceSearchChoiceChange = this.handleAdvanceSearchChoiceChange.bind(this);
 	}
 
 	componentDidMount() {
-		fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lon}&exclude=${"minutely"}&appid=${this.apiKeyWeather}`)
-			.then(data => data.json())
-			.then(data => this.setState({ weather_data: data }));
 	}
 
 	handleLocationSearchChange(new_value) {
-		this.setState({ location_name: (new_value.trim() === "" ? "" : new_value) })
+		this.setState({ location_name: new_value });
 	}
 
 	handleLocationSearchSubmit() {
-		//if searching form is empty, then open advanced searching window
 		if (this.state.location_name.trim() === "") {
+			//if searching form is empty, then open advanced searching window
 			this.setState({
 				advanced_search: {
 					show: true,
 					chosen_record_index: 0,
+					temp_location_data: [],
 				}
 			});
 		}
-		//if searching form isnt empty, then try to fetch data
 		else {
-			this.setState({ loading_location_data: true }, () => {
-				fetch(`https://eu1.locationiq.com/v1/search.php?key=${this.apiKeyLocation}&q=${this.state.location_name}&format=json&limit=20`)
-					.then(response => {
-						if (response.ok)
-							return response.json();
-						else{
-							console.log(response.body)
-							throw Error(response.statusText);
-						}
-						// console.log(response);
-					})
-					.then(data => {
-						//if location api query returned error
-						if ("error" in data) {
-							this.setState(state => ({
-								loading_location_data: false,
-								errors: state.errors.concat([{ message: data.error }]),
-							}));
-						}
-						else {
-							this.setState(state => ({
-								temp_location_data: data,
-								location_data: (data.length === 1 ? data[0] : state.data),
-								loading_location_data: false,
-								advanced_search: {
-									show: (data.length <= 1 ? state.advanced_search.show : true),	//if theres more than one record fetched, then advanced searcg window is shown
-									chosen_record_index: 0,
-								}
-							}));
-						}
-					})
-					.catch(error => console.log(error.message));//this.handleErrorAdd("Connection error while trying to fetch location data")); //fetch() throws exceptions only on connection errors (so error in catch = no internet connection)
-			});
+			//if searching form isnt empty, then try to fetch data
+			this.fetchLocationData();
 		}
+	}
+
+	fetchLocationData() {
+		this.setState({ loading_location_data: true }, () => {
+			fetch(`https://eu1.locationiq.com/v1/search.php?key=${this.apiKeyLocation}&q=${this.state.location_name}&format=json&limit=20`)
+				.then(response => {
+					if (response.ok) {
+						return response.json();
+					}
+					else {
+						throw Error(response.statusText);
+					}
+				})
+				.then(data => {
+					//if only one location available, fetch weather data immediately
+					if (data.length === 1) {
+						this.setState({
+							loading_location_data: false,
+							temp_location_data: data,
+							location_data: data[0],
+						}, this.fetchWeatherData);	//fetch weather data when state change
+					}
+					else {
+						//if more locations available, open advanced search window
+						this.setState({
+							loading_location_data: false,
+							temp_location_data: data,
+							advanced_search: {
+								show: true,
+								chosen_record_index: 0,
+							}
+						})
+					}
+				})
+				.catch(error => {
+					console.log("Location API error: " + error.message);
+					this.handleErrorAdd("Location API error: " + error.message);
+				});
+		});
+	}
+
+	fetchWeatherData() {
+		this.setState({ loading_weather_data: true }, () => {
+			fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.location_data.lat}&lon=${this.state.location_data.lon}&exclude=${"minutely"}&appid=${this.apiKeyWeather}`)
+				.then(response => {
+					if (response.ok) {
+						return response.json();
+					}
+					else {
+						throw Error(response.statusText);
+					}
+				})
+				.then(data => this.setState({
+					weather_data: data,
+					loading_weather_data: false
+				}))
+				.catch(error => {
+					console.log("Weather API error: " + error.message);
+					this.handleErrorAdd("Weather API error: " + error.message)
+				})
+		})
 	}
 
 	handleAdvanceSearchChoiceChange(new_value) {
@@ -117,14 +144,14 @@ class App extends React.Component {
 		});
 	}
 
-	handleAdvanceSearchSave() {
+	handleAdvanceSearchSubmit() {
 		this.setState(state => ({
 			advanced_search: {
 				show: false,
 				chosen_record_index: 0,
 			},
 			location_data: state.temp_location_data[state.advanced_search.chosen_record_index],
-		}), () => console.log(this.state.location_data, this.state.temp_location_data));
+		}), this.fetchWeatherData);	//fetch weather data when state change
 	}
 
 	handleErrorDelete() {
@@ -132,7 +159,7 @@ class App extends React.Component {
 	}
 
 	handleErrorAdd(err_message) {
-		console.log(`Error: ${err_message}`)
+		// console.log(`Error: ${err_message}`)
 		this.setState(state => ({
 			loading_weather_data: false,
 			loading_location_data: false,
@@ -154,7 +181,7 @@ class App extends React.Component {
 
 				<AdvancedSearchWindow
 					onCancel={this.handleAdvanceSearchCancel}
-					onSave={this.handleAdvanceSearchSave}
+					onSave={this.handleAdvanceSearchSubmit}
 					show={this.state.advanced_search.show}
 					onSearchChange={this.handleLocationSearchChange}
 					onSearchSubmit={this.handleLocationSearchSubmit}
@@ -173,9 +200,20 @@ class App extends React.Component {
 
 
 				{
-					true
-						// this.state.weather_data !== undefined && this.state.location !== undefined
+					this.state.loading_weather_data === true
 						?
+						
+						<div className="d-flex w-100 justify-content-center align-items-center" style={{ height: "calc(100vh - 62px)" }}>
+							<BS.Spinner animation="border" variant="info" size="lg" />
+						</div>
+						:
+						<>
+							<div style={{ whiteSpace: "pre", display: "flex" }}>
+								<div style={{ width: "50%", boxSizing: "border-box" }}>{JSON.stringify(this.state.weather_data, null, 2)}</div>
+								<hr />
+								<div style={{ width: "50%", boxSizing: "border-box" }}>{JSON.stringify(this.state.location_data, null, 2)}</div>
+							</div>
+						</>
 						// <BS.Container fluid>
 						// 	<BS.Row>
 						// 		<BS.Col lg={{span: 8, offset: 2}} md={{span: 10, offset: 1}}>
@@ -183,17 +221,6 @@ class App extends React.Component {
 						// 		</BS.Col>
 						// 	</BS.Row>
 						// </BS.Container>
-						<>
-							<div style={{ whiteSpace: "pre", display: "flex" }}>
-								<div style={{ width: "50%", boxSizing: "border-box", border: "1px solid aqua" }}>{JSON.stringify(this.state.weather_data, null, 2)}</div>
-								<hr />
-								<div style={{ width: "50%", boxSizing: "border-box" }}>{JSON.stringify(this.state.location_data, null, 2)}</div>
-							</div>
-						</>
-						:
-						<div className="d-flex w-100 justify-content-center align-items-center" style={{ height: "calc(100vh - 62px)" }}>
-							<BS.Spinner animation="border" variant="info" size="lg" />
-						</div>
 				}
 			</>);
 	}
